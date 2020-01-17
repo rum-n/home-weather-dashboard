9b565b6570856ccab85a9f81e732fb3e8b2439e9
rum-n <manev.ru@gmail.com>
2020-01-17 16:22:54 +0200

    ski conditions

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ window.addEventListener('load', ()=> {
     let tempDegree = document.querySelector('.temp-degree');
     let locationTz = document.querySelector('.location-tz');
 
+    let vitoshaTemp = document.querySelector('.vitTemp');
+    let vitoshaWind = document.querySelector('.vitWind');
+    let vitoshaRain = document.querySelector('.vitRain');
+    let vitoshaSum = document.querySelector('.vitSum');
+
+    let rilaTemp = document.querySelector('.rilTemp');
+    let rilaWind = document.querySelector('.rilWind');
+    let rilaRain = document.querySelector('.rilRain');
+    let rilaSum = document.querySelector('.rilSum');
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
@@ -37,4 +47,43 @@ window.addEventListener('load', ()=> {
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
     }
+
+    function vitosha (){
+        const proxy = 'https://cors-anywhere.herokuapp.com';
+        const api = `${proxy}/https://api.darksky.net/forecast/7bac0ec31ccb6313d3c1db5e94c867fd/42.582537,23.292406?units=si`;
+        
+        fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temperature, summary, windSpeed, precipProbability} = data.currently;
+                vitoshaTemp.textContent = Math.round(temperature);
+                vitoshaSum.textContent = summary;
+                vitoshaWind.textContent = windSpeed;
+                vitoshaRain.textContent = precipProbability;
+             });
+    }
+
+    function rila (){
+        const proxy = 'https://cors-anywhere.herokuapp.com';
+        const api = `${proxy}/https://api.darksky.net/forecast/7bac0ec31ccb6313d3c1db5e94c867fd/42.239547,23.325955?units=si`;
+        
+        fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temperature, summary, windSpeed, precipProbability} = data.currently;
+                rilaTemp.textContent = Math.round(temperature);
+                rilaSum.textContent = summary;
+                rilaWind.textContent = windSpeed;
+                rilaRain.textContent = precipProbability;
+             });
+    }
+
+    vitosha();
+    rila();
 });
